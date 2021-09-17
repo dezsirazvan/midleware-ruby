7fc0faaf31091eab16f87916c1b35394f34e2b67
Dezsi Razvan <dezsirazvan@ES-HQ.home>
2021-09-17 19:14:23 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Midle
     config.middleware.insert(0, Rack::ReverseProxy) do
       reverse_proxy_options preserve_host: false
       reverse_proxy_options force_ssl: true, replace_response_host: true
-      reverse_proxy '/', 'https://104.22.68.176:443'
+      reverse_proxy '/', 'https://bitcoin.org/'
       # reverse_proxy '/', 'http://82.78.94.148:80'
     end
     # Configuration for the application, engines, and railties goes here.
