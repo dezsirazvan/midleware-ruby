e0ad47b8a9afa2a41538fdc0bffdf8b2a0c8aea9
Dezsi Razvan <dezsirazvan@ES-HQ.home>
2021-09-17 18:37:36 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Midle
     config.middleware.insert(0, Rack::ReverseProxy) do
       reverse_proxy_options preserve_host: false
       reverse_proxy_options force_ssl: true, replace_response_host: true
-      reverse_proxy '/', 'https://www.coinbase.com/'
+      reverse_proxy '/', 'https://www.google.com/'
     end
     # Configuration for the application, engines, and railties goes here.
     #
